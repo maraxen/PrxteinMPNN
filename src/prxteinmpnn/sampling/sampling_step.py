Fix hyperparameter unpacking and update loss calculation in sample_straight_through_estimator_step

src/prxteinmpnn/sampling/sampling_step.py
@@ -176,7 +176,7 @@ def sample_straight_through_estimator_step(
       carry,
 
   """
-  learning_rate, target_logits = hyperparameters[0], hyperparameters[1]
+  learning_rate, _target_logits = hyperparameters[0], hyperparameters[1]
   current_key, edge_features, node_features, _sequence, current_logits = carry
   (
     node_features,
@@ -188,8 +188,6 @@ def sample_straight_through_estimator_step(
   ) = sample_model_pass_fn_only_prng(
     current_key,
   )
-
-  jax.debug.print("➡️ Iteration {_i}, Mask Sum: {s}", _i=_i, s=jnp.sum(mask))
 
   @jax.jit
   def loss_fn(input_logits: Logits) -> tuple[CEELoss, tuple[NodeFeatures, Logits]]:
@@ -206,7 +204,7 @@ def sample_straight_through_estimator_step(
     )
     output_logits = final_projection(model_parameters, updated_node_features)
 
-    return ste_loss(output_logits, target_logits, mask), (updated_node_features, output_logits)
+    return ste_loss(output_logits, input_logits, mask), (updated_node_features, output_logits)
 
   (_, (new_node_features, _)), grad = jax.value_and_grad(loss_fn, has_aux=True)(
     current_logits,
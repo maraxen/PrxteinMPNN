5794f93629b19e1dc6e4e09d981f00461db51ac4
Marielle Russo <67157875+maraxen@users.noreply.github.com>
2025-08-12 16:35:10 -0400

    Add SamplingConfig to module exports in sampling package

--- src/prxteinmpnn/sampling/__init__.py
+++ src/prxteinmpnn/sampling/__init__.py
@@ -2,9 +2,10 @@
 
 from . import initialize, sample, sampling_step, ste
 from .sample import make_sample_sequences
-from .sampling_step import SamplingEnum
+from .sampling_step import SamplingConfig, SamplingEnum
 
 __all__ = [
+  "SamplingConfig",
   "SamplingEnum",
   "initialize",
   "make_sample_sequences",
